cdcb2725aea55e6ede90035ad7c520da66bd92c1
Shashank Neelagiri <sneelagiri@outlook.com>
2020-04-09 13:39:51 +0200

    yarn add and app.use cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import * as superagent from "superagent";
+import cors from "cors";
 
 const app: express.Application = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 
 const API_KEY: string = "659d5c6b8f3d2447f090119e48202fdb";
